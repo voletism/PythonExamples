c8d8a0e969a6e1243e9897826d5b62263fd67d02
Subramanya Murthy <smsvoleti@gmail.com>
2018-12-21 15:05:47 +0200

    rename variables

--- beyondCompare.py
+++ beyondCompare.py
@@ -4,46 +4,46 @@ import os
 def beyondCompare(fname1, fname2):
 
     # Open file for reading in text mode (default mode)
-    with open(fname1, 'r') as f1, open(fname2, 'r') as f2:
+    with open(fname1, 'r') as file1, open(fname2, 'r') as file2:
 
         print("-----------------------------------")
         print("Compare files", "F> " + fname1, "<S " + fname2)
         print("-----------------------------------")
 
         # Read the first line from the files
-        f1_line = f1.readline()
-        f2_line = f2.readline()
+        file1_line = file1.readline()
+        file2_line = file2.readline()
 
         # line number
         line_num = 1
 
         # Loop if either file1 or file2 has not reached EOF
-        while f1_line != '' or f2_line != '':
+        while file1_line != '' or file2_line != '':
 
             # Strip the leading whitespaces
-            f1_line = f1_line.rstrip()
-            f2_line = f2_line.rstrip()
+            file1_line = file1_line.rstrip()
+            file2_line = file2_line.rstrip()
 
             # Compare the lines from both file
-            if f1_line != f2_line:
+            if file1_line != file2_line:
 
                 # If a line does not exist on file2 then mark the output with + sign
-                if f2_line == '' and f1_line != '':
-                    print("F>+","%d"% line_num,f1_line)
+                if file2_line == '' and file1_line != '':
+                    print("F>+","%d"% line_num,file1_line)
                 #
-                elif f1_line != '':
-                    print("F>","%d"% line_num,f1_line)
+                elif file1_line != '':
+                    print("F>","%d"% line_num,file1_line)
 
                 # If a line does not exist on file1 then mark the output with + sign
-                if f1_line == '' and f2_line != '':
-                    print("<S+","%d"% line_num,f2_line)
+                if file1_line == '' and file2_line != '':
+                    print("<S+","%d"% line_num,file2_line)
                 #
-                elif f2_line != '':
-                    print("<S","%d"% line_num,f2_line)
+                elif file2_line != '':
+                    print("<S","%d"% line_num,file2_line)
 
             # Read the next line from the file
-            f1_line = f1.readline()
-            f2_line = f2.readline()
+            file1_line = file1.readline()
+            file2_line = file2.readline()
 
             # Increment line number
             line_num += 1
